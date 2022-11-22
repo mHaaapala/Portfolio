6db57823758662ddba2b80c360c088d60ff8329e
mHaaapala <mHaaapala@users.noreply.github.com>
2022-11-22 08:43:03 +0200

    hej

---
+++ components/TopBar.tsx
@@ -0,0 +1,13 @@
+import styles from './TopBar.module.css'
+
+function TopBar() {
+  return (
+    <div>
+      <h1 className=" text-white bg-gradient-to-r from-red-500">
+        <p>About me</p>
+      </h1>
+    </div>
+  )
+}
+
+export default TopBar;
