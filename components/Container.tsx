4a6514a7dc9267e7026b7eae66e304367b3c370e
mHaaapala <mHaaapala@users.noreply.github.com>
2022-11-29 10:46:13 +0200

    123

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,11 +4,8 @@ function Container() {
   return (  
   <div className="max-w-screen-lg mx-auto p-4">
     <div className="grid grid-cols-2 gap-4 mt-12 h-5/6">
-      <div className="shadow-lg pb-full rounded-3xl bg-gray-100 bg-opacity-50">
-
-      </div>
-      <div className="shadow-lg pb-full rounded-3xl bg-gray-100 bg-opacity-50">
-
+      <div className=" shadow-lg pb-full rounded-3xl bg-gray-100 bg-opacity-50">
+          <p className="font-bold text-slate-600 text-center">Programmer</p>
       </div>
     </div>
   </div>
