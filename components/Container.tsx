038c864513241bdb0cf63a9f8b5f3cbec115d9cb
mHaaapala <mHaaapala@users.noreply.github.com>
2022-12-07 08:49:04 +0200

    213

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,7 +5,7 @@ function Container() {
   <div className="max-w-screen-lg mx-auto p-4">
     <div className="grid mt-12">
       <div className="shadow-lg pb-full rounded-3xl bg-gray-100 bg-opacity-50">
-          <p className="font-bold text-slate-600 text-center">Programmer</p>
+          <p className="font-bold text-slate-600 text-center"></p>
       </div>
     </div>
   </div>
