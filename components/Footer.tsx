908b55c747666f390379711e573558fd6e936c33
mHaaapala <mHaaapala@users.noreply.github.com>
2023-04-28 12:43:53 +0300

    123

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,9 +8,9 @@ const Footer = () => {
         <img className="w-6" src="/cp.png"></img>Copyright 2023
       </div>
       <div className="flex">
-        <a href="https://github.com/mHaaapala" target="_blank"><img src="/github.png" className="w-20"></img></a>
-        <a href="https://www.instagram.com/kalnab3" target="_blank"><img src="/iglogo.png" className="w-20"></img></a>
-        <a href="https://twitter.com/kalnab3" target="_blank"><img src="/twitter.png" className="w-20"></img></a>
+        <Link href="https://github.com/mHaaapala"><img src="/github.png" className="w-20"></img></Link>
+        <Link href="https://www.instagram.com/kalnab3"><img src="/iglogo.png" className="w-20"></img></Link>
+        <Link href="https://twitter.com/kalnab3"><img src="/twitter.png" className="w-20"></img></Link>
       </div>
       <div className="">
         <h5>Contact</h5>
--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 function TopBar() {
   return (
       <div className=" bg-gradient-to-r from-red-400 to-blue-400 flex">
-        <a href="/"><img className="w-20" src="/home.png"></img></a>
+        <Link href="/"><Image className="w-20" width={400} height={400} alt={"logo"} src="/home.png"></Image></Link>
         <Link className="text-black my-6 mx-14" href="/contact">  <p>Socials</p> </Link>
         <Link className="text-black my-6" href="/projects"> <p>Projects</p> </Link>
       </div>
