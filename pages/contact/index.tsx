30ba02515d64500b2dc08fa935f65ce5d8b09686
mHaaapala <mHaaapala@users.noreply.github.com>
2023-04-28 12:38:15 +0300

    redeploy

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -11,9 +11,9 @@ const info = () => {
     <div className="grid mt-12">
       <div className=" pb-full rounded-3xl bg-gray-100 bg-opacity-50">
           <p className="flex justify-center w-full text-2xl text-zinc-400 text-center">
-              <a href="https://twitter.com/kalnab3" target="_blank"><img className="w-32" src="/twitter.png"></img>Twitter</a>
-              <a href="https://www.instagram.com/kalnab3" target="_blank"><img className="w-32 mx-44" src="/iglogo.png"></img>Instagram</a>
-              <a href="https://github.com/mHaaapala" target="_blank"><img className="w-32" src="/github.png"></img>Github</a>
+              <a href="https://twitter.com/kalnab3"><img className="w-32" src="/twitter.png"></img>Twitter</a>
+              <a href="https://www.instagram.com/kalnab3"><img className="w-32 mx-44" src="/iglogo.png"></img>Instagram</a>
+              <a href="https://github.com/mHaaapala"><img className="w-32" src="/github.png"></img>Github</a>
           </p>
       </div>
     </div>
